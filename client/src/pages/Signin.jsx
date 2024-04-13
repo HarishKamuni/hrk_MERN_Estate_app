17301a8133f6cbbb2490bf0766aa9b0e3afc017c
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2024-04-14 00:24:45 +0530

    add Redux Toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
+import { signInFailure, signInStart, signInSuccess } from '../user/userSlice';
 
 const Signin = () => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
-  const [err, setErr] = useState('');
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
+  // const [err, setErr] = useState('');
+  // const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -17,7 +21,8 @@ const Signin = () => {
     e.preventDefault();
     // console.log(formData);
     try {
-      setLoading(true);
+      // setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -28,16 +33,19 @@ const Signin = () => {
       const data = await res.json();
       // console.log(data.success);
       if (data.success === false) {
-        setLoading(false);
-        setErr(data.message);
+        // setLoading(false);
+        // setErr(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setErr(null);
+      // setLoading(false);
+      // setErr(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setErr(error.message);
+      // setLoading(false);
+      // setErr(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
@@ -75,7 +83,7 @@ const Signin = () => {
           <span className="text-blue-700">signup</span>
         </Link>
       </div>
-      {err && <p className="text-red-500">{err}</p>}
+      {error && <p className="text-red-500">{error}</p>}
     </div>
   );
 };
